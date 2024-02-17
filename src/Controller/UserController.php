43a0f22bba364e3ebeef4471901d026f67ffbe09
Herold7 <makpier@gmail.com>
2024-02-17 19:31:15 +0100

    Ajout des routes afin d'exécuter la mise à jour des formulaires des clients

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,60 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ClientType;
+use App\Service\ClientService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/complete-profile', name: 'complete_profile')]
+    public function index(
+        Request $request,
+        ClientService $ClientService,
+        EntityManagerInterface $em
+    ): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        $form = $this->createForm(ClientType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $ClientService->updateProfile($form, $this->getUser(), $em);
+            
+            $this->addFlash('success', 'Your profile has been updated');
+            return $this->redirectToRoute('account');
+        }
+        return $this->render('user/client.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * User account route for displaying it's own data on the app
+     */
+    #[Route('/account', name: 'account', methods: ['GET', 'POST'])]
+    public function account(
+        Request $request,
+        EntityManagerInterface $em,
+        ClientService $ClientService
+    ): Response
+    {
+        if(!$this->getUser()->getUserIdentifier()) {
+            return $this->redirectToRoute('complete_profile');
+        }
+
+        $form = $this->createForm(ClientType::class, $this->getUser());
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $ClientService->updateProfile($form, $this->getUser(), $em);
+            $this->addFlash('success', 'Your profile has been updated');
+            return $this->redirectToRoute('account');
+        }
+        return $this->render('user/account.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
