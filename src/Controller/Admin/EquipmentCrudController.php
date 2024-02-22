07f32818877181845d74d17d6ddf30c0da3f6268
Herold7 <makpier@gmail.com>
2024-02-22 10:08:05 +0100

    🔥 Suppresion de codes inutiles

--- src/Controller/Admin/EquipmentCrudController.php
+++ src/Controller/Admin/EquipmentCrudController.php
@@ -10,7 +10,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class EquipmentCrudController extends AbstractCrudController
