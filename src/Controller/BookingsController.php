b099cf524096b0edd8f18284c31fc6d4e4f407f8
Efrem YOUSSOUF <youssouf.efrem@gmail.com>
2024-02-27 16:07:13 +0100

    Implémentation de la recherche filtrée par dates et logiciels dans le controller et le repository.

--- src/Controller/BookingsController.php
+++ src/Controller/BookingsController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\BookingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use DateTime;
 
 class BookingsController extends AbstractController
 {
     #[Route('/bookings', name: 'app_bookings')]
-    public function index(): Response
+    public function index(Request $request, BookingRepository $bookingRepository): Response
     {
+        
+        $startDateString = $request->query->get('startDate');
+        $endDateString = $request->query->get('endDate');
+
+        
+        $startDate = DateTime::createFromFormat('Y-m-d', $startDateString);
+        $endDate = DateTime::createFromFormat('Y-m-d', $endDateString);
+
+        
+        $bookings = $bookingRepository->findBookingsByDates($startDate, $endDate);
+
         return $this->render('bookings/index.html.twig', [
             'controller_name' => 'BookingsController',
+            'bookings' => $bookings,
         ]);
     }
 }
