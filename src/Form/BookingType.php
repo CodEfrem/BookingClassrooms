9739367c131809bd4b26e171f4fe6af669ba7a0f
Herold7 <makpier@gmail.com>
2024-02-27 14:12:51 +0100

    🐛 Suppression des propriétés d'entités inutiles dans le formulaire de réservation

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -5,14 +5,15 @@ namespace App\Form;
 use App\Entity\User;
 use App\Entity\Booking;
 use App\Entity\Classroom;
-use App\Entity\Customer; // Importez Customer
 use Symfony\Component\Form\AbstractType;
+use App\Entity\Customer; // Importez Customer
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType; 
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType; // Utilisez ChoiceType pour le choix
@@ -22,41 +23,24 @@ class BookingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'name', 
-                'label' => 'Nom de l\'utilisateur ',
-                'disabled' => true, 
-            ])
-            ->add('customers', EntityType::class, [ // Changez ChoiceType à EntityType
-                'class' => Customer::class, // Spécifiez la classe Customer
-                'choice_label' => 'id', // Changez cela selon vos besoins
-                'label' => 'Clients', // Changez cela selon vos besoins
-                'multiple' => true, // Permettre la sélection multiple si nécessaire
-            ])
             ->add('start_date', DateType::class, [
-                'label' => 'Date de début de la réservation ',
+                'label' => 'Date de début de la réservation',
                 'widget' => 'single_text',
-                'attr' => ['min' => date('Y-m-d')],
+                'attr' => ['min' => date('d-m-Y')],
             ])
             ->add('end_date', DateType::class, [
-                'label' => 'Date de fin de la réservation ',
+                'label' => 'Date de fin de la réservation',
                 'widget' => 'single_text',
-                'attr' => ['min' => date('Y-m-d')],
+                'attr' => ['min' => date('d-m-Y')],
             ])
-            ->add('number', TextType::class, [
-                'label' => 'Numéro de téléphone  ',
-            ])
-            ->add('amount', MoneyType::class, [
-                'label' => 'Montant de la réservation ',
-                'currency' => 'EUR',
-                'disabled' => true,  
+            ->add('customers', IntegerType::class, [
+                'label' => 'Nombre de clients',
             ])
             ->add('classroom', EntityType::class, [ 
                 'class' => Classroom::class,
                 'choice_label' => 'name ', 
                 'label' => 'Salle n° ',
-                'disabled' => true, 
+                'disabled' => false, 
             ])
             ;
     }
